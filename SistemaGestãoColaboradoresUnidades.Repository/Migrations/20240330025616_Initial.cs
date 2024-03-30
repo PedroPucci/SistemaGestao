f9ab1b76eaef90fcae7283ee6a295cb523c92546
pedro.h.pucci <pedro.pucci@mutant.com.br>
2024-03-30 17:59:14 -0300

    correcao

---
+++ SistemaGestãoColaboradoresUnidades.Repository/Migrations/20240330025616_Initial.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SistemaGestãoColaboradoresUnidades.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UnityEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<int>(type: "integer", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Inactivated = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnityEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CollaboratorEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    UnityEntityId = table.Column<int>(type: "integer", nullable: false),
+                    UserEntityId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CollaboratorEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CollaboratorEntity_UnityEntity_UnityEntityId",
+                        column: x => x.UnityEntityId,
+                        principalTable: "UnityEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CollaboratorEntity_UserEntity_UserEntityId",
+                        column: x => x.UserEntityId,
+                        principalTable: "UserEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollaboratorEntity_UnityEntityId",
+                table: "CollaboratorEntity",
+                column: "UnityEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollaboratorEntity_UserEntityId",
+                table: "CollaboratorEntity",
+                column: "UserEntityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CollaboratorEntity");
+
+            migrationBuilder.DropTable(
+                name: "UnityEntity");
+
+            migrationBuilder.DropTable(
+                name: "UserEntity");
+        }
+    }
+}
